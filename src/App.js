07fc089466ff1fee8dfe0ee9cc18bec368118bf6
Joshua D'Souza <joshua@Joshuas-MacBook-Pro.local>
2022-11-17 13:53:56 +0300

    deploy tutorial

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   const [isPageDark, setIsPageDark] = useState(false);
   return (
     <div className={ isPageDark ? "BodyDark" : "BodyLight" }>
-        <img alt="Profile Pic" className="ProfileImage" src="https://media-exp1.licdn.com/dms/image/C4D03AQHMmkp3U8vQdA/profile-displayphoto-shrink_800_800/0/1646000571951?e=1658361600&v=beta&t=_DPW_lHvwdHVNzlcJCfdrL3d74OqEHlbBS4PE6t-vRM" />
+        <img alt="Profile Pic" className="ProfileImage" 
+        src="https://media-exp1.licdn.com/dms/image/C4D03AQHMmkp3U8vQdA/profile-displayphoto-shrink_400_400/0/1646000571951?e=1674086400&v=beta&t=y5ROVjSUbcvieEcOD3pVwL-QcTS0lmWvzDkyHvI_IUk"
+        />
         <h1>
             Joshua D'Souza :) Thank you!
         </h1>
@@ -13,7 +15,7 @@ function App() {
             Hey everyone, I'm Josh!
         </h3>
         <p>
-            This is an example of an HTML page. Thanks for doing this tutorial!
+            This is an example of a React page. Thanks for doing this tutorial!
         </p>
         <a className={ isPageDark ? "LightText" : "DarkText" } id="linkedin" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/joshua-d-souza-758b74132/">
             My LinkedIn
